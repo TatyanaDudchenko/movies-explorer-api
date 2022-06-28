0a8b87e4eb7cb040c3d1b66519f16b5788d71c57
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-29 02:30:00 +0500

    feat: настроить аутентификацию и авторизацию

---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+// const JWT_SECRET = 'themostclassifiedsecretsecret';
+
+const getToken = (id) => jwt.sign({ id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+
+const isAuthtorized = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new AuthorizationError('Необходима авторизация')); // 401
+    return;
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = await jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    next(new AuthorizationError('Необходима авторизация')); // 401
+    return;
+  }
+
+  req.user = payload;
+
+  next();
+};
+
+module.exports = {
+  getToken,
+  isAuthtorized,
+};
