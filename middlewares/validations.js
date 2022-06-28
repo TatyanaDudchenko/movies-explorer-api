706f2ea6749ac2eaae949f31820f89a7745e6528
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-29 03:18:26 +0500

    feat: реализовать валидацию

---
+++ middlewares/validations.js
@@ -0,0 +1,63 @@
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+
+// const urlRegExp = /^(http(s)?):\/\/(w{3}.)?[а-яА-Я\w\-._~:\/?#[\]@!$&'()*+,;=]+$/;
+
+const validateURL = (value) => {
+  if (!validator.isURL(value, { require_protocol: true })) {
+    throw new Error('Неправильный формат ссылки');
+  }
+  return value;
+};
+
+const validationsCreateUser = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+  }),
+});
+
+const validationsUpdateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const validationsLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validationsCreateMovie = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom(validateURL),
+    trailerLink: Joi.string().required().custom(validateURL),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().custom(validateURL),
+  }),
+});
+
+const validationsDeleteMovieByID = celebrate({
+  // валидируем параметры
+  params: Joi.object().keys({
+    movieId: Joi.string().length(24).hex().required(),
+  }),
+});
+
+module.exports = {
+  validationsCreateUser,
+  validationsUpdateUser,
+  validationsLogin,
+  validationsCreateMovie,
+  validationsDeleteMovieByID,
+};
