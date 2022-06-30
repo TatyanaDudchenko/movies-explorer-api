9b6694c8a0f0743a242c9df9ea9d9230715421a9
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-07-01 01:11:07 +0500

    fix: исправить порт 4000 на 3000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const { routes } = require('./routes/app');
